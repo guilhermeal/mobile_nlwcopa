1569a660d4203779561a1c18ea96510a2b86822a
Guilherme Almeida <guiallan.al@gmail.com>
2022-11-04 23:36:44 -0300

    feat: create project mobile
    install libs and configure first steps

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.900"  >
+      <Text color="#F9F9F9" fontFamily="heading" fontSize={24} >
+        SignIn
+      </Text>
+    </Center>
+  )
+}
