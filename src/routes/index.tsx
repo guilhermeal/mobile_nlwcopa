2fd4b1fdf15f9d604807ef68c65fa8995dc03eb8
Guilherme Almeida <guiallan.al@gmail.com>
2022-11-06 03:13:08 -0300

    feat: set backend api access
    get user data from api including token on headers and redirect users to other routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
+
 import { AppRoutes } from './app.routes';
+import { SignIn } from '../screens/SignIn';
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <AppRoutes />
+      <SignIn />
     </NavigationContainer>
   )
 }
