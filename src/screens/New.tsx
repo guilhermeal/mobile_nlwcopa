cd7ac7d59674793ae0b8106a0c7298cf8a080fcc
Guilherme Almeida <guiallan.al@gmail.com>
2022-11-06 04:09:54 -0300

    feat(New): create new poll

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,11 +1,53 @@
-import { Heading, VStack, Text } from 'native-base';
+import { Heading, VStack, Text, Toast, useToast } from 'native-base';
 import { Header } from '../components/Header';
 
 import Logo from '../assets/logo.svg';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
+import { useState } from 'react';
+import { api } from '../services/api';
 
 export function New() {
+
+  const [title, setTitle] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+
+  async function handlePollCreate() {
+    if(!title.trim()) {
+      return toast.show({
+        title: 'Informe um nome para o seu bolão',
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    }
+
+    try {
+      setIsLoading(true);
+
+      await api.post('/polls', { title })
+
+      toast.show({
+        title: 'Bolão criado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500'
+      });
+
+      setTitle('');
+
+    } catch(error) {
+      console.log(error);
+
+      toast.show({
+        title: 'Não foi possível criar seu bolão',
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <VStack flex={1} bgColor='gray.900' >
 
@@ -20,9 +62,18 @@ export function New() {
           e compartilhe entre amigos!
         </Heading>
 
-        <Input mb={2} placeholder='Qual nome do seu bolão?' />
+        <Input 
+          mb={2} 
+          placeholder='Qual nome do seu bolão?' 
+          onChangeText={setTitle}
+          value={title}
+        />
 
-        <Button title='CRIAR MEU BOLÃO' />
+        <Button 
+          title='CRIAR MEU BOLÃO' 
+          onPress={handlePollCreate}
+          isLoading={isLoading}
+        />
 
         <Text color="gray.200" fontSize="md" textAlign="center" px={10} mt={4}>
           Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas.
