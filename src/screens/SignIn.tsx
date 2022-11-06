9c32e3fee631d5db48532a24c051803d92c448cb
Guilherme Almeida <guiallan.al@gmail.com>
2022-11-06 03:23:16 -0300

    feat: redirect user in other routes

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -7,7 +7,7 @@ import { Button } from '../components/Button';
 
 export function SignIn() {
 
-  const { signIn, user } = useAuth();
+  const { signIn, isUserLoading } = useAuth();
 
   return (
     <Center 
@@ -24,6 +24,12 @@ export function SignIn() {
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         mt={12}
         onPress={signIn}
+        isLoading={isUserLoading}
+        _loading={{
+          _spinner: {
+            color: 'white'
+          }
+        }}
       />
 
       <Text color='white' textAlign='center' mt={4}>
